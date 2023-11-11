58d5017050c7ee1cf14ec836c25d0f7c8ec284dd
Sony Jacob Thomas <sonyjtp@gmail.com>
2023-11-11 14:56:39 -0600

    RangeSumBST.kt

---
+++ src/main/kotlin/leetcode/tree/ZigZagLevelOrderTraversal.kt
@@ -0,0 +1,29 @@
+//https://tinyurl.com/leetcode023
+package leetcode.tree
+
+import leetcode.tree.pojo.TreeNode
+import java.util.*
+
+/** doesn't work - memory limit exceeded */
+fun zigzagLevelOrderBfs(root: TreeNode<Int>?): List<List<Int>> {
+    root ?: return listOf()
+    val result = mutableListOf<List<Int>>()
+    val nodes = LinkedList<TreeNode<Int>>()
+    var nodesInSameLevel = LinkedList<Int>()
+    var fromLeft = true
+    nodes.addLast(root)
+    nodes.addLast(null)
+    while (nodes.isNotEmpty()) {
+        nodes.pollFirst()?.let { currNode ->
+            if (fromLeft) nodesInSameLevel.addLast(currNode.`val`) else nodesInSameLevel.addFirst(currNode.`val`)
+            currNode.left?.let { nodes.addLast(it) }
+            currNode.right?.let { nodes.addLast(it) }
+        } ?: run {
+            result.add(nodesInSameLevel)
+            nodesInSameLevel = LinkedList<Int>()
+            if (nodes.isNotEmpty()) nodes.addLast(null)
+            fromLeft = !fromLeft
+        }
+    }
+    return result
+}
---
+++ src/main/kotlin/leetcode/tree/bst/RangeSumBST.kt
@@ -0,0 +1,21 @@
+//https://tinyurl.com/leetcode024
+package leetcode.tree.bst
+
+import leetcode.tree.pojo.TreeNode
+
+
+fun rangeSumBST(root: TreeNode<Int>?, low: Int, high: Int): Int {
+    var sum = 0
+    fun bst(root: TreeNode<Int>?){
+        root?: return
+        if (root.`val` < low) { bst(root.right) }
+        else if (root.`val` > high) { bst(root.left) }
+        else {
+            sum += root.`val`
+            bst(root.left)
+            bst(root.right)
+        }
+    }
+    bst(root)
+    return sum
+}
